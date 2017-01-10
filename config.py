96b1ba69b9dddb90f7e3af0490db462cf4bd4f88
Qsbbq <qavila@f45c89c74e43.ant.amazon.com>
2017-01-10 15:15:16 -0800

    added variables

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+import os
+db_name = os.environ['RDS_DB_NAME']
+db_user = os.environ['RDS_USERNAME']
+db_password = os.environ['RDS_PASSWORD']
+db_host = os.environ['RDS_HOSTNAME']
+db_port = os.environ['RDS_PORT']
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@flask-demo.co1s6zhqgeco.us-west-2.rds.amazonaws.com:3306/demodb'.format(db_user, db_password)
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
